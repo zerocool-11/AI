64d438db747a651312a8417c93be2fa1ea2c59e4
root <root@localhost.localdomain>
2020-05-21 02:41:02 +0530

    clear

--- program.py
+++ program.py
@@ -13,17 +13,10 @@ from keras.layers import MaxPooling2D
 
 model = Sequential()
 
-model.add(Convolution2D(filters=32, 
-                        kernel_size=(3,3), 
-                        activation='relu',
-                   input_shape=(64, 64, 3)
-                       ))
+model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',input_shape=(64, 64, 3)))
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Convolution2D(filters=32, 
-                        kernel_size=(3,3), 
-                        activation='relu',
-                       ))
+model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',))
 
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
