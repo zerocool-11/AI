9367b8c4e5872134a941c6f3b3f1623390b967c2
root <root@localhost.localdomain>
2020-05-23 18:05:30 +0530

    v4  use it with input.txt

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l2
 from keras.datasets import mnist
